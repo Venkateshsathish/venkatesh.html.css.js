0bf398611023a68af1f8cadc85d9bdf96c057ffe
Venkatesh <117707313+Venkateshsathish@users.noreply.github.com>
2022-12-02 18:45:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-
-let person={
-    name:"venkatesh",
-    age:21,
-    gender:"male",
-    place:"tirunelveli",
-
+for(let i=0;i<=10;i++){
+    console.log("#Number"+i);
 }
-console.log(person)
+
+
+
+
+let moblie=40000;
+switch(true){
+    case( moblie>55000):  
+    console.log("Iphone");
+    break;
+    case (moblie >30000):
+        console.log("android");
+        break;
+        case (moblie>5000):
+            console.log("basics");
+            break;
+          default:
+            console.log("you can't afford moblie phone");
+          
+}
