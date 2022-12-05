8a94d5ce699c9ba5314117c7e927ddd1c95a447a
Venkatesh <117707313+Venkateshsathish@users.noreply.github.com>
2022-12-05 18:44:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
+//<--dote Notation  object-->
 let person={
-   name:"sathish",
-   age:27,
-   intrest:"palying foodball and cricket",
-   stats :"married",
-greeting:function () {
-    let  mgs='Hi  my  name is   '  +this.name+ 'my hoby ' + this.intrest;
-console.log(mgs)
+    name: 'venkatesh',
+   // age : 21,
+    gender:'male'
 }
-};
-console.log(person.greeting());
+console.log(person.age);
+//<--barcket notation object-->
+console.log(person['name']);
 
 
-
-
-
-
-
-
-
-//<----do-while--->
-let i=1;
-do {
-    if (i % 2 !==0 ) {
-        console.log("Number#"+i);
-
+let age=21
+if(1<18){
+    console.log(true);
+}
+    else{
+        console.log(false);
     }
-    i++;
-}while(i<=20)
-//<----for in---->
-/*const person ={
-    name :'venkatesh',
-    age: 21,
-    sex:'male'
 
-};
-for( let key in person){
-    console.log(key+";",person[key]);
-}
-*/
+
